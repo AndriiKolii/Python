59dba23416d38c05140bdc9f729a114751466369
AndriiKolii <kolii.andriy@gmail.com>
2023-03-08 21:40:42 +0200

    print course name print name, surname, record book number print lecturer name and surname 45 times print calculation result (variant 7)

--- kolii task1.py
+++
@@ -1,7 +0,0 @@
-print('Multi-paradigm languages: Task 1')
-print('Andrii Kolii FI-9119')
-print('Vladyslav Ovsianikov, ' * 44, 'Vladyslav Ovsianikov')
-result = ((15 + 11.1/3) / (33 + 12.3)) - 123.1
-result = round(result, 3)
-print(result)
-
