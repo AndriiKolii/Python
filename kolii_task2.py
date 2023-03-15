4b179bce464b31df1dae8253dc81e4d37ceaaf79
AndriiKolii <kolii.andriy@gmail.com>
2023-03-15 10:15:50 +0200

    print course name; student name, surname, record book number input values and calculate result

---
+++ kolii_task2.py
@@ -0,0 +1,12 @@
+# Мультипарадигмальні мови програмування, лабораторна 2, Андрій Колій, FI-9119
+
+print('Multi-paradigm languages: Task 1')
+print('Andrii Kolii FI-9119')
+
+x = int(input('X: '))
+y = int(input('Y: '))
+z = int(input('Z: '))
+
+result = (15 + x / y) / (33 + x) - z
+result = round(result, 3)
+print(result)
